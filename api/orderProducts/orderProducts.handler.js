49cff7bd4c1e11260a50ccb69e459466ae9532f1
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-09-01 12:22:06 -0300

    Update orderProducts.handler.js

--- api/orderProducts/orderProducts.handler.js
+++ api/orderProducts/orderProducts.handler.js
@@ -16,16 +16,24 @@ async function getOrderProduct(id) {
 async function createOrderProduct(data) {
     const order = (await ordersHandler.getOrders()).find(e => e.id == data.orderId);
     const products = await productsHandler.getProducts();
-    if(order == undefined){
-        return {message: "This order doesn't exists!"}
+    const orderProductsId = data.productsId;
+    if (order == undefined) {
+        return { message: "This order doesn't exists!" }
     }
 
-    for(const product of products){
-        for(){
-            
+    for (const product of products) {
+        let find = true;
+        for (const orderProductId of orderProductsId) {
+            if (product.id == orderProductId.productId) {
+                find = false;
+            }
         }
-        console.log(product);
     }
+    if (find == true) {
+        
+    }
+
+    // return await crud.save(tableName, null, data);
 
 }
 
